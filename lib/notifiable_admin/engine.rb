be7bf94cc0975dee1d650cbe56ea7d8666473229
Matt Brooke-Smith <matt@futureworkshops.com>
2015-02-27 16:12:36 +0100

    Fix engine tests

--- lib/notifiable_admin/engine.rb
+++ lib/notifiable_admin/engine.rb
@@ -22,11 +22,11 @@ module NotifiableAdmin
     
     # append migrations
     initializer :append_migrations do |app|
-      unless app.root.to_s.match root.to_s
+      #unless app.root.to_s.match root.to_s
         config.paths["db/migrate"].expanded.each do |expanded_path|
           app.config.paths["db/migrate"] << expanded_path
         end
-      end
+      #end
     end
   end
 
