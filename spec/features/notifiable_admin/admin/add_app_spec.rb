e051c874aded7ead3368d2a7061cf86da90ddc80
Matt Brooke-Smith <matt@futureworkshops.com>
2015-06-29 13:28:08 +0100

    Update spec

--- spec/features/notifiable_admin/admin/add_app_spec.rb
+++ spec/features/notifiable_admin/admin/add_app_spec.rb
@@ -8,11 +8,12 @@ feature 'add app' do
     login_as account_owner, :scope => :admin
     visit notifiable_admin.new_admin_account_app_path(account_owner.account) 
     fill_in 'app_apns_certificate', :with => 'abc123'
-    
+    fill_in 'app_gcm_api_key', :with => 'zxy456'    
     click_button 'Create'
     
     expect(page).to have_content("App was created")
     expect(Notifiable::App.count).to eq 2
     expect(Notifiable::App.last.configuration[:apns][:certificate]).to eql 'abc123'
+    expect(Notifiable::App.last.configuration[:gcm][:api_key]).to eql 'zxy456'
   end
 end
