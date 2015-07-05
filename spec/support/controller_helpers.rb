9232de24de6375ce50e670206368ed84ec212036
Matt Brooke-Smith <matt@futureworkshops.com>
2015-07-05 23:22:34 +0100

    Update helpers

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,11 +1,18 @@
 module ControllerHelpers
-   def admin_sign_in(admin = double('admin'))
-     if admin.nil?
-       allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :admin})
-       allow(controller).to receive(:current_admin).and_return(nil)
-     else
-       allow(request.env['warden']).to receive(:authenticate!).and_return(admin)
-       allow(controller).to receive(:current_admin).and_return(admin)
-     end
-   end
+  def admin_sign_in(admin = double('admin'))
+    if admin.nil?
+      allow(request.env['warden']).to receive(:authenticate!).and_throw(:warden, {:scope => :admin})
+      allow(controller).to receive(:current_admin).and_return(nil)
+    else
+      allow(request.env['warden']).to receive(:authenticate!).and_return(admin)
+      allow(controller).to receive(:current_admin).and_return(admin)
+    end
+  end
+end
+
+# Raise exceptions that occur
+ActionController::Base.class_eval do
+  def rescue_action(exception)
+    raise exception
+  end
 end
