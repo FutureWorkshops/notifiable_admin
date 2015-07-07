ad9c4ce733597c1da338eea7dda2d3b45ef00bb8
Matt Brooke-Smith <matt@futureworkshops.com>
2015-07-07 10:15:03 +0100

    Update spec

--- spec/model/notifiable/device_token_spec.rb
+++ spec/model/notifiable/device_token_spec.rb
@@ -2,22 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Notifiable::DeviceToken, :type => :model do
   
-  describe "#properties" do
+  describe "#settings" do
     
     context "new value" do
-      subject { create(:apns_token, :device_name => "Matt's iPhone") }
-      
-      before(:each) { subject.settings['onsite'] = true; subject.save }
-      
+      subject { create(:apns_token, :onsite => true) }
+            
       it { expect(subject.settings['onsite']).to eq true }
     end
     
-    context "filter value" do
-      subject { create(:apns_token) }
-      
-      before(:each) { subject.settings['onsite'] = "true"; subject.save }
-      
-      it { byebug; expect(Notifiable::DeviceToken.where("settings -> 'onsite' = 'true'").count).to eq 1 }
+    context "filter" do
+      subject! { create(:apns_token, :onsite => true) }
+            
+      it { expect(Notifiable::DeviceToken.where("settings -> 'onsite' = 'true'").count).to eq 1 }
     end
     
   end
