536191e8f14dd36cc691a22b16145bb0b7917333
Matt Brooke-Smith <matt@futureworkshops.com>
2015-07-08 23:17:14 +0100

    Update notification decorator

--- spec/test_app/app/decorators/models/notifiable/notification_decorator.rb
+++ spec/test_app/app/decorators/models/notifiable/notification_decorator.rb
@@ -1,13 +1,44 @@
 Notifiable::Notification.class_eval do
   
-  def enqueue_send(user)
-    self.batch do
-      if user
-        self.add_notifiable(user)
-      else
-        self.app.device_tokens.each {|d| self.add_device_token(d) } 
-      end
-    end    
+  def delay_public(run_at = nil)
+    delayable(run_at).send_public
   end
+  
+  def delay_private(user, run_at = nil)
+    delayable(run_at).send_private(user)
+  end
+  
+  def delay_filtered(filters, run_at = nil)
+    delayable(run_at).send_filtered(filters)
+  end
+  
+  private
+    def delayable(run_at)
+      self.delay(:app_id => self.app.id, :notification_id => self.id, :run_at => run_at)
+    end
+  
+    def send_filtered(filters)
+      
+      device_tokens = self.app.device_tokens
+      filters.each_pair do |key,value|
+        device_tokens = device_tokens.where("custom_properties -> '#{key}' = '#{value}'")
+      end
+      
+      self.batch do
+        device_tokens.each {|d| self.add_device_token(d) } 
+      end 
+    end
+  
+    def send_private(user)
+      self.batch do
+        user.device_tokens.each {|d| self.add_device_token(d) } 
+      end 
+    end
+  
+    def send_public
+      self.batch do
+        self.app.device_tokens.each {|d| self.add_device_token(d) } 
+      end 
+    end
   
 end
