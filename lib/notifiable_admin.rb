fa5d7684ed24772bd06c782b627b0208aaefbc44
Matt Brooke-Smith <matt@futureworkshops.com>
2015-07-16 11:46:58 +0100

    Add notifiable_admin configuration

--- lib/notifiable_admin.rb
+++ lib/notifiable_admin.rb
@@ -2,4 +2,10 @@ require 'notifiable_admin/engine'
 
 module NotifiableAdmin
 
+  mattr_accessor :custom_device_token_properties
+  @@custom_device_token_properties = {}
+  
+  def self.configure
+    yield self
+  end
 end
