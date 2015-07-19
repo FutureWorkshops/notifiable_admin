629ebfca1be92d036b5eb446ab539bea2df0d289
Matt Brooke-Smith <matt@futureworkshops.com>
2015-07-19 08:54:09 +0100

    Update jobs index
    Fix message display for errors

--- spec/features/notifiable_admin/admin/list_jobs_spec.rb
+++ spec/features/notifiable_admin/admin/list_jobs_spec.rb
@@ -12,18 +12,31 @@ feature 'List jobs' do
   let(:notification) { create(:notification, :app => notifiable_app) }
   let!(:localized_notification) { create(:localized_notification, :notification => notification, :locale => :en)}
 
-  before(:each) { notification.delay_private(@user, 5.hours.from_now) }
+  before(:each){ notification.delay_private(@user, 5.hours.from_now) }
 
-  context "As account owner" do
+  context "account owner" do
     before(:each) { login_as account_owner, :scope => :admin }
     
-    it "shows a single job" do    
-      visit notifiable_admin.admin_account_app_jobs_path(account, notifiable_app)
-
-      expect(current_path).to eq notifiable_admin.admin_account_app_jobs_path(account, notifiable_app)
-      expect(page).to have_css(".job-row", :count=>1)
-    end    
+    context "scheduled" do
+      before(:each) { visit notifiable_admin.admin_account_app_jobs_path(account, notifiable_app) }
+      
+      it { expect(current_path).to eq notifiable_admin.admin_account_app_jobs_path(account, notifiable_app) }
+      it { expect(page).to have_css(".job-row", :count=>1) }
+    end
+    
+    context "with error" do
+      before(:each) do 
+        Delayed::Job.first.update_attribute(:last_error, "A very long error message that will need to be truncated because its very long winded and doesnt get to the point quickly instead it beats around the bush.")
+      
+        visit notifiable_admin.admin_account_app_jobs_path(account, notifiable_app) 
+      end
+    
+      it { expect(current_path).to eq notifiable_admin.admin_account_app_jobs_path(account, notifiable_app) }    
+      it { expect(page).to have_css(".job-row", :count=>1) } 
+    end
+       
   end
+  
 
   
 end
