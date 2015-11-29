ebf848659c8ad013d6f06efe67745fe1bf14b7e6
Matt Brooke-Smith <matt@futureworkshops.com>
2015-11-29 14:52:30 +0000

    Only apply hstore to postgres

--- db/migrate/20150622164050_add_hstore.rb
+++ db/migrate/20150622164050_add_hstore.rb
@@ -1,9 +1,19 @@
 class AddHstore < ActiveRecord::Migration
+  
+  def is_connection_postgres?
+    connection.adapter_name.downcase.to_sym == :postgresql
+  end
+  
   def up
-    enable_extension :hstore
+    if is_connection_postgres?
+      enable_extension :hstore    
+    else
+      Rails.logger.warn("Can only enable hstore on postgres")
+    end
+    
   end
 
   def down
-    disable_extension :hstore
+    disable_extension :hstore if is_connection_postgres? 
   end
 end
