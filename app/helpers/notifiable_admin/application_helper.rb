43452ceeba2c5b9310135352af58c7330a04ee03
Matt Brooke-Smith <matt@futureworkshops.com>
2016-01-22 23:39:18 +0000

    Update helpers

--- app/helpers/notifiable_admin/application_helper.rb
+++ app/helpers/notifiable_admin/application_helper.rb
@@ -1,11 +1,5 @@
 module NotifiableAdmin::ApplicationHelper
   
-  def current_app
-    return nil if current_admin.super_admin?
-    
-    params[:app_id] ? @app : current_admin.account.apps.first
-  end
-  
   # Building blocks
   def navbar_item(name, path, active = false, method = :get)
     html_options = active ? {:class => :active} : {}
@@ -37,6 +31,10 @@ module NotifiableAdmin::ApplicationHelper
     button admin_account_app_notification_notification_statuses_path(@account, notification.app, notification), "Receipts", "ok-circle"
   end
   
+  def new_app_button
+    button new_admin_account_app_path(@account), "New App", "plus"
+  end
+  
   def edit_app_button(app)
     button edit_admin_account_app_path(@account, app), "Edit", "pencil"
   end
@@ -46,40 +44,36 @@ module NotifiableAdmin::ApplicationHelper
     navbar_item("Accounts", super_admin_accounts_path, params[:controller] == "super_admin/accounts" && params[:action] == "index")
   end
     
-  def device_tokens_navbar_item(current_app)
-    navbar_item("Device Tokens", current_app.new_record? ? "/" : admin_app_device_tokens_path(current_app), params[:controller] == "admin/device_tokens" && params[:action] == "index")    
+  def device_tokens_navbar_item
+    navbar_item("Device Tokens", @app.new_record? ? "/" : admin_app_device_tokens_path(@app), params[:controller] == "admin/device_tokens" && params[:action] == "index")    
   end
   
   def notifications_navbar_item
-    navbar_item("Notifications", admin_account_app_notifications_path(@account, current_app), params[:controller] == "admin/notifications" && params[:action] == "index")
+    navbar_item("Sent", admin_account_app_notifications_path(@account, @app), params[:controller] == "admin/notifications" && params[:action] == "index")
   end
   
   def app_users_navbar_item
-    navbar_item("App Users", admin_account_app_users_path(@account, current_app), params[:controller] == "admin/users" && params[:action] == "index")
-  end
-  
-  def app_device_tokens_navbar_item
-    navbar_item("App Devices", admin_account_app_device_tokens_path(@account, current_app), params[:controller] == "admin/device_tokens" && params[:action] == "index")    
+    navbar_item("Devices", admin_account_app_device_tokens_path(@account, @app), params[:controller] == "admin/device_tokens" && params[:action] == "index")
   end
   
   def app_jobs_navbar_item
-    navbar_item("Schedule", admin_account_app_jobs_path(@account, current_app), params[:controller] == "admin/jobs" && params[:action] == "index")    
+    navbar_item("Scheduled", admin_account_app_jobs_path(@account, @app), params[:controller] == "admin/jobs" && params[:action] == "index")    
   end
   
   def admins_navbar_item
-    navbar_item("Account Users", admin_account_admins_path(@account), params[:controller] == "admin/admin" && params[:action] == "index")
+    navbar_item("Account", admin_account_admins_path(@account), params[:controller] == "admin/admin" && params[:action] == "index")
   end
   
   def new_app_navbar_item
-    navbar_item("Add App", new_admin_account_app_path(@account), params[:controller] == "admin/apps" && params[:action] == "new")
+    navbar_item("New App", new_admin_account_app_path(@account), params[:controller] == "admin/apps" && params[:action] == "new")
   end
   
   def edit_app_navbar_item
-    navbar_item("App Settings", edit_admin_account_app_path(@account, @app), params[:controller] == "admin/apps" && params[:action] == "edit")
+    navbar_item("Settings", edit_admin_account_app_path(@account, @app), params[:controller] == "admin/apps" && params[:action] == "edit")
   end
 
   def api_keys_navbar_item
-    navbar_item("API Keys", admin_account_notifications_api_users_path(@account), params[:controller] == "admin/notifications_api_users" && params[:action] == "index")   
+    navbar_item("Notification API Keys", admin_account_notifications_api_users_path(@account), params[:controller] == "admin/notifications_api_users" && params[:action] == "index")   
   end
   
   def api_documentation_navbar_item
@@ -92,6 +86,10 @@ module NotifiableAdmin::ApplicationHelper
   
   def app_navbar_item(app)
     navbar_item(app.name, admin_account_app_path(@account, app), params[:controller] == "admin/apps" && params[:action] == "show" && params[:id].to_i == app.id)
+  end
+  
+  def app_send_public_notification_navbar_item
+    navbar_item "Compose", new_admin_account_app_notification_path(@account, @app)
   end
   
   # Paths
