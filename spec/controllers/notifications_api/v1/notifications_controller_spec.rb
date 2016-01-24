674f219936f79105f771a228fdf39fc3eaab6178
Matt Brooke-Smith <matt@futureworkshops.com>
2016-01-24 02:03:16 +0000

    Update specs

--- spec/controllers/notifications_api/v1/notifications_controller_spec.rb
+++ spec/controllers/notifications_api/v1/notifications_controller_spec.rb
@@ -6,9 +6,7 @@ describe NotifiableAdmin::NotificationsApi::V1::NotificationsController do
   let(:n_app) { create(:app, :account => account, :name => "The Open") }
   let(:api_user) { create(:notifications_api_user, :account => account, :apps => [n_app]) }
     
-  before(:each) do    
-    allow(controller).to receive(:authenticate_from_headers!) { controller.instance_variable_set(:@current_user, api_user) }
-  end
+  before(:each) { ApiAuth.sign!(request, api_user.access_id, api_user.secret_key) }
   
   describe "#create" do
 
@@ -25,9 +23,19 @@ describe NotifiableAdmin::NotificationsApi::V1::NotificationsController do
     context "with params" do
       before(:each) { post :create, {:app_id => n_app.id, :notification => {:localized_notifications_attributes => [{:message => "Hello", :locale => :en, :params => {:video_id => "123"}}]}} }
 
+      it { expect(response.status).to eq 200 }
       it { expect(Notifiable::Notification.count).to eq 1 }
       it { expect(Notifiable::Notification.first.localized_notifications.count).to eq 1 }
       it { expect(Notifiable::Notification.first.localized_notifications.first.params['video_id']).to eq "123" }
+    end
+    
+    context "disabled" do
+      before(:each) do 
+        api_user.update_attribute(:enabled, false)
+        post :create, {:app_id => n_app.id, :notification => {:localized_notifications_attributes => [{:message => "Hello", :locale => :en, :params => {:video_id => "123"}}]}}
+      end
+
+      it { expect(response.status).to eq 403 }
     end
     
     context "filtered" do
@@ -46,6 +54,7 @@ describe NotifiableAdmin::NotificationsApi::V1::NotificationsController do
         }        
       end
 
+      it { expect(response.status).to eq 200 }
       it { expect(Notifiable::Notification.count).to eq 1 }
       it { expect(Notifiable::Notification.first.sent_count).to eq 1 }
     end
--- spec/features/notifiable_admin/admin/delete_notifications_api_user_spec.rb
+++ spec/features/notifiable_admin/admin/delete_notifications_api_user_spec.rb
@@ -5,7 +5,7 @@ feature 'Delete Notifications API User' do
   let(:account) { create(:account) }  
   let(:account_owner) { create(:account_owner, :account => account) }
 
-  let!(:notifications_api_user) { create(:notifications_api_user, :account => account) }
+  let!(:notifications_api_user) { create(:notifications_api_user, :account => account, :enabled => false) }
     
   before(:each) { login_as account_owner, :scope => :admin }    
   
@@ -14,7 +14,7 @@ feature 'Delete Notifications API User' do
     click_on 'Delete'
     
     expect(NotifiableAdmin::NotificationsApiUser.count).to eq 0
-    expect(page).to have_content "Notifications API Key deleted"
+    expect(page).to have_content "Key deleted"
     expect(current_path).to eql notifiable_admin.admin_account_notifications_api_users_path(account)
   end
   
