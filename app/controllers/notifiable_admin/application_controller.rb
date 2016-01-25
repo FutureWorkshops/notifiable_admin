9d25b9588fff463053bd02de4984df4e70caf758
Matt Brooke-Smith <matt@futureworkshops.com>
2016-01-25 00:17:27 +0000

    Update application_controller
    Only include application_helper if it exists

--- app/controllers/notifiable_admin/application_controller.rb
+++ app/controllers/notifiable_admin/application_controller.rb
@@ -1,5 +1,6 @@
 class NotifiableAdmin::ApplicationController < ActionController::Base
   helper NotifiableAdmin::ApplicationHelper
+  helper ::ApplicationHelper if File.exists?(File.join(Rails.root, "app", "helpers", "application_helper.rb"))
   
   protect_from_forgery with: :exception
 
