e0101a3ead011de9f55b2fd1b56f5ca221f2d2e4
Matt Brooke-Smith <matt@futureworkshops.com>
2016-02-14 22:31:07 +0000

    Update device_tokens_controller
    Only show valid tokens

--- app/controllers/notifiable_admin/admin/device_tokens_controller.rb
+++ app/controllers/notifiable_admin/admin/device_tokens_controller.rb
@@ -4,7 +4,7 @@ class NotifiableAdmin::Admin::DeviceTokensController <NotifiableAdmin::Admin::Ba
   load_and_authorize_resource :device_token, :class => "Notifiable::DeviceToken", :through => :app
 
   def index
-    @device_tokens = @device_tokens.page(params[:page])
+    @device_tokens = @device_tokens.where(:is_valid => true).page(params[:page])
   end
   
 end
