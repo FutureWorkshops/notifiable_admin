1e8cacd0f10a5eebb72c9651229e7c3ae279077b
Matt Brooke-Smith <matt@futureworkshops.com>
2016-03-13 17:42:13 +0000

    Update apps#form
    Remove gcm_api_key

--- spec/features/notifiable_admin/admin/add_app_spec.rb
+++ spec/features/notifiable_admin/admin/add_app_spec.rb
@@ -8,12 +8,10 @@ feature 'add app' do
     login_as account_owner, :scope => :admin
     visit notifiable_admin.new_admin_account_app_path(account_owner.account) 
     fill_in 'app_apns_certificate', :with => 'abc123'
-    fill_in 'app_gcm_api_key', :with => 'zxy456'    
     click_button 'Save'
     
     expect(page).to have_content("App was created")
     expect(Notifiable::App.count).to eq 2
     expect(Notifiable::App.last.configuration[:apns][:certificate]).to eql 'abc123'
-    expect(Notifiable::App.last.configuration[:gcm][:api_key]).to eql 'zxy456'
   end
 end
