99d822233c516ddfd87b691aad98fe6ad325c768
Matt Brooke-Smith <matt@futureworkshops.com>
2016-04-08 22:41:38 +0100

    Fix date_format

--- config/initializers/date_format.rb
+++ config/initializers/date_format.rb
@@ -1,4 +1,4 @@
 Time::DATE_FORMATS.merge!(
-  :default => lambda { |time| time.today? ? time.strftime("%H:%m") : time.strftime("#{time.day.ordinalize} %h, %H:%m") },
-  :seconds => lambda { |time| time.today? ? time.strftime("%H:%m:%S") : time.strftime("#{time.day.ordinalize} %h, %H:%m:%S") }
+  :default => lambda { |time| time.today? ? time.strftime("%H:%M") : time.strftime("#{time.day.ordinalize} %h, %H:%M") },
+  :seconds => lambda { |time| time.today? ? time.strftime("%H:%M:%S") : time.strftime("#{time.day.ordinalize} %h, %H:%M:%S") }
 )
